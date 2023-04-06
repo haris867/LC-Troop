500252f56ceebe72943e6448af1a1083b4057976
Allawi <allawi465@gmail.com>
2023-04-06 19:13:10 +0200

    using anime from anime.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import anime from './node_modules/animejs/lib/anime.es.js';
+
+anime({
+    targets: '#logo-svg path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 2500,
+    delay: 1500,
+    direction: 'alternate',
+    loop: false,
+    complete: () => {
+        anime({
+            targets: "path",
+            fill: '#ffffff',
+            duration: 1500,
+            easing: 'easeInOutQuad',
+        });
+    }
+});
